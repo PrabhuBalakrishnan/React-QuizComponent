3e1edfa57df05acadd26fd63caee9792525b78d6
PrabhuBalakrishnan <63336763+PrabhuBalakrishnan@users.noreply.github.com>
2020-04-08 13:56:55 +0530

    fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,10 @@ class Quiz extends Component {
         super(props);
         this.state = { quiz_position: 1 }
     }
-    //const isQuizEnd = quizData.quiz_questions.length === state.quiz_position - 1;
 
     render() {
+        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 5;
+
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
