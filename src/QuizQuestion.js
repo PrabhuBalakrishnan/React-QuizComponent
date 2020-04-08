eb585200eb2761c99365a9c990c9be7f37f6c873
PrabhuBalakrishnan <63336763+PrabhuBalakrishnan@users.noreply.github.com>
2020-04-08 14:18:09 +0530

    test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,7 +11,11 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {this.props.quiz_question.answer_options.map((answer_option, index) =>
+                            (<QuizQuestionButton handleClick={} button_text={answer_option} key={index} />)
+                        )
+                        }
+
                     </ul>
                 </section>
             </main>
